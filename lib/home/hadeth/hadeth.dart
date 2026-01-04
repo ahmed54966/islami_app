f1e6fa1bd49a60257a46835756834f0dec7e03cd
Ahmed Mahmoud <am4402211@gmail.com>
2026-01-05 01:04:49 +0200

    added ahadeth.txt

--- lib/home/hadeth/hadeth.dart
+++ lib/home/hadeth/hadeth.dart
@@ -1,11 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:islami/home/colors.dart';
 
-class HadethTab extends StatelessWidget{
+
+class HadethTab extends StatefulWidget{
+  @override
+  State<HadethTab> createState() => _HadethTabState();
+}
+
+class _HadethTabState extends State<HadethTab> {
+List <Hadeth> ahadethList = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+
+    loadHadethFile();
+
+
+    return Column(
+      children: [
+        Expanded(child: Image.asset("assets/images/hadeth_logo.png")),
+        Divider(
+          color: AppColors.primaryLightColor,
+          thickness: 3,
+        ),
+        Text("اسم الحديث",style: Theme.of(context).textTheme.bodyMedium,),
+        Divider(
+          color: AppColors.primaryLightColor,
+          thickness: 3,
+        ),
+        Expanded(
+          flex: 2,
+          child:ahadethList.isEmpty?
+          Center(child: CircularProgressIndicator(color: AppColors.primaryLightColor,),):
+          ListView.separated(
+            separatorBuilder: (context,index){
+              return Divider(
+          color: AppColors.primaryLightColor,
+          thickness: 2,
+        );
+            },
+            itemBuilder: (context,index){
+                return Text(ahadethList[index].title);
+            },
+            itemCount:ahadethList.length,
+          ),
+        )
+
+
+
+      ],
     );
   }
+
+  void loadHadethFile()async{
+  String hadethContent = await rootBundle.loadString('assets/files/ahadeth.txt');
+  List <String> hadethList =   hadethContent.split("#\r\n");
+  for(int i = 0 ; i<hadethList.length ; i++){
+  List <String> hadethLines =  hadethList[i].split("\n");
+  String title = hadethLines[0];
+  hadethLines.removeAt(0);
+  Hadeth hadeth = Hadeth(title: title, content: hadethLines);
+  ahadethList.add(hadeth);
+  setState(() {
+    
+  });
+  }
+  }
+}
+class Hadeth {
+  String title;
+  List <String> content ;
+  Hadeth({required this.title,required this.content});
 }
